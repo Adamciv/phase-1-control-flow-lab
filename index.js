6287f9341883ba51289f6022ed0aa82fd81e5c40
ahmedj <ahmedarifusa@gmail.com>
2024-05-09 12:45:19 +0300

    last update

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+  let y
+  if (x<=400){
+    const message = 'This one is on me!'
+    return message
+  } else if (x>400 && x<2000){
+    return "That will be twenty bucks."
+  }else if (x>2000 && x<2500){
+    return "I will gladly take your thirty bucks."
+  } else if (x>2500) {
+    return "No can do."
+  } 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(name){
   // Write your code here!
+
+    return name === 'NYC' ? 'Ok, sounds good.':'No go.';
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(x){
   // Write your code here!
+
+  switch(true){
+    case x === 'generous':
+    return 'Thank you so much.';
+
+    case x === 'regular':
+    return 'Thank you.';
+
+    default:
+    return 'Bye.';
+  }
 }
